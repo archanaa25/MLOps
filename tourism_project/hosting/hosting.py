a71b9c00ada3b7ee509d3cc9bfb3d5321b649dd3
archanaa25 <31532399+archanaa25@users.noreply.github.com>
2026-04-11 01:04:07 +0400

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,10 +1,25 @@
-from huggingface_hub import HfApi
+
+from huggingface_hub import HfApi, create_repo
+from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
+
+repo_id = "arss25/Tourism-Package-Purchase-Prediction"
+repo_type = "space"
+
+# Step 1: Check if the space exists
+try:
+    api.repo_info(repo_id=repo_id, repo_type=repo_type)
+    print(f"Space '{repo_id}' already exists. Using it.")
+except RepositoryNotFoundError:
+    print(f"Space '{repo_id}' not found. Creating new space...")
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    print(f"Space '{repo_id}' created.")
+
 api.upload_folder(
     folder_path="tourism_project/deployment",     # the local folder containing your files
-    repo_id="arss25/Tourism-Package-Purchase-Prediction",          # the target repo
-    repo_type="space",                      # dataset, model, or space
+    repo_id=repo_id,          # the target repo
+    repo_type=repo_type,                      # dataset, model, or space
     path_in_repo="",                          # optional: subfolder path inside the repo
 )
