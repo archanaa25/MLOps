97b887dee4113b16078d569e2623cda1b179dcfd
archanaa25 <31532399+archanaa25@users.noreply.github.com>
2026-04-11 09:00:47 +0400

    Update hosting.py

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,4 +1,3 @@
-
 from huggingface_hub import HfApi, create_repo
 from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 import os
@@ -14,7 +13,7 @@ try:
     print(f"Space '{repo_id}' already exists. Using it.")
 except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
+    create_repo(repo_id=repo_id, repo_type=repo_type, private=False, space_sdk='streamlit')
     print(f"Space '{repo_id}' created.")
 
 api.upload_folder(
